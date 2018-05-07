e466ae1ce57c45b9f9c723f7195fae6a6fa7ae0a
takasehideki <takase@i.kyoto-u_ac_jp>
2018-05-07 19:23:17 +0900

    Add plug to deps and Modify version

--- phoenix/benchmarker/mix.exs
+++ phoenix/benchmarker/mix.exs
@@ -28,11 +28,12 @@ defmodule Benchmarker.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
+    [{:phoenix, "~> 1.3.2"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
+     {:phoenix_html, "~> 2.10"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:plug, "~> 1.3"}]
   end
 end
